9ab0e4e3be16338b2069be6d1fb401e57186d375
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-18 14:55:54 +0100

    add html markup with for each cicle

--- assets/script.js
+++ assets/script.js
@@ -36,25 +36,31 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
-
-markup = `
- <div class="col">
-   <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">This is a wider card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.</p>
-         <p class="card-text"><small class="text-body-secondary">Last updated 3 mins
-         ago</small></p>
-       </div>
-      </div>
-        
+let memberCard = ''
+const rowEl = document.querySelector('.row')
+teamMembers.forEach(element => {
+  
+  const {name, role, email, img} = element
+  const markup = `
+   <div class="col">
+     <div class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${img}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+         <div class="card-body">
+           <h5 class="card-title">${name}</h5>
+           <p class="card-text">${role}</p>
+           <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+         </div>
+        </div>
+          
+        </div>
       </div>
     </div>
-  </div>
-`
+  `
+  memberCard += markup
+});
+
+rowEl.innerHTML = memberCard
